56a450e01a753d0e835e4ea45e980411a2b514b5
Tu Le Thanh <lethanhtu1551998@gmail.com>
2018-03-19 22:50:33 +0700

    them 2 file tao stack su dung 2 cach khac nhau

---
+++ Data struct/stack_use_dynamic_array.cpp
@@ -0,0 +1,73 @@
+﻿// chương trình stack sử dụng dynamic array 
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+// cấu trúc dữ liệu của stack sử dụng mảng động
+typedef struct DynStack {
+	int top;
+	int capacity;
+	int *Arr;
+} DynStack;
+int IsArrayFull(DynStack *S)
+{
+	if (S->top == S->capacity-1)
+		return 1;
+	else
+		return 0;
+}
+int IsArrayEmpty(DynStack *S)
+{
+	if (S->top == -1)
+		return 1;
+	else
+		return 0;
+}
+// hàm gấp đôi sức chưa của array 
+void DouleArray(DynStack *S)
+{
+	S->capacity = S->capacity*2;
+	S->Arr = (int *)realloc(S->Arr, S->capacity* sizeof(int)); // cấp phát bộ nhớ cho mảng mới 
+}
+void push(DynStack *S, int data)
+{
+	if (IsArrayFull(S) == 1)
+		DouleArray(S);
+	
+		S->top++;
+		S->Arr[S->top] = data;
+	
+		
+
+}
+void pop(DynStack *S)
+{
+	if (IsArrayEmpty(S) == 1)
+		cout << "Stack is empty ";
+	else
+		S->top--;
+}
+void Display(DynStack *S)
+{
+	for(int i=0; i<= S->top; i++)
+		cout << S->Arr[i] << endl;
+	system("pause");
+}
+int main()
+{
+	DynStack *S = new DynStack;
+	S->top = -1;
+	S->capacity = 1;
+	S->Arr = (int *)malloc( S->capacity * sizeof(int));
+	int n;
+	cout << "\nNhap so phan tu ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x; 
+		cout << "\nNhap gia tri ";
+		cin >> x;
+		push(S, x);
+	}
+	Display(S);
+	return 0;
+}
---
+++ Data struct/strack_use_linked_list.cpp
@@ -0,0 +1,67 @@
+﻿// chương trình stack sử dụng linked list 
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+typedef struct ListNode {
+	int data;
+	struct ListNode *next;
+}ListNode;
+
+int IsEmptyStack(ListNode *head)
+{
+	if (head == NULL)
+		return 1;
+	else
+		return 0;
+}
+void Push(ListNode **head, int data)
+{
+	ListNode *temp = new ListNode;
+	temp->data = data;
+	temp->next = *head;
+	*head = temp;
+}
+void Pop(ListNode **head)
+{
+	if (IsEmptyStack(*head) == 1)
+		cout << "Stack is empty";
+	else
+	{
+		ListNode *temp = new ListNode;
+		temp = *head;
+		*head = (*head)->next;
+		delete(temp);
+	}
+	
+}
+void Display(ListNode **head)
+{
+	ListNode *temp = new ListNode;
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		cout << temp->data << endl;;
+		temp = temp->next;
+	}
+	cout << temp->data << endl;
+	
+}
+
+int main()
+{
+	ListNode *head = NULL;
+	int n;
+	cout << "\nNhap n ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cout << "\nNhap x ";
+		cin >> x;
+		Push(&head, x);
+	}
+	Display(&head);
+	system("pause");
+	return 0;
+}
